640fd539664a93c01e06c9c7e05efb84882b6304
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2022-10-08 17:28:08 +0400

    added basic header + timer

---
+++ app/src/components/game/HackHeader/HackHeader.tsx
@@ -0,0 +1,24 @@
+import React, {useContext, useMemo} from 'react';
+import { ShowByState } from '_components/misc/ShowByState/ShowByState';
+import { Timer } from '_components/misc/Timer/Timer';
+import { HackStateContext, useDispatchContext } from '_contexts/hack';
+import actions from '_contexts/hack/actions';
+import styles from './styles.module.scss';
+
+
+export const HackHeader: React.FC = () => {
+  const dispatch = useDispatchContext();
+  const state = useContext(HackStateContext);
+
+  const loseGame = () => dispatch({type: actions.END, payload: 'fail'});
+
+  return useMemo(() => (
+    <div className={styles.root}>
+      {state.visible
+      ? <ShowByState condition={state.locked} placeholder={'breaching protocol acquired'}>
+          <Timer className={styles.timer} seconds={100} onEnd={loseGame}/>
+        </ShowByState>
+      : <span>system mode</span>}
+    </div>
+  ), [state.locked, state.visible])
+}
