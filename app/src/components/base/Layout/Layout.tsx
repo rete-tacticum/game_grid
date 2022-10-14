b093e6a107adea155185d75ab6b99e62227f7694
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2022-10-14 21:17:42 +0400

    layout styling

--- app/src/components/base/Layout/Layout.tsx
+++ app/src/components/base/Layout/Layout.tsx
@@ -9,7 +9,7 @@ export const Layout = ({header, main, aside, footer, background, className}: Lay
       <header className={styles.header}>{ header }</header>
       <div className={styles.content}>
         <main className={styles.main}>{ main }</main>
-        <aside className={styles.aside}>{ aside }</aside>
+        {aside && <aside className={styles.aside}>{ aside }</aside>}
       </div>
       <footer className={styles.footer}>{ footer }</footer>
       <div className={styles.background}>{ background }</div>
